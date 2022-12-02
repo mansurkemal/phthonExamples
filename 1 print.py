70f8e2b47adc7c1b5e2b32e23d538c739a1e8889
mansurkemal <mansurkemal@hotmail.com>
2022-12-03 02:19:54 +0300

    hersey

--- 1 print.py
+++ 1 print.py
@@ -1,6 +1,31 @@
-#metin yazd�rma
+#metin yazd�rma
 print("hello world!")
 
-#degisken yazd�rma
+#degisken yazd�rma
 metin = "Yenimahalle Fen"
 print(metin)
+mesaj="SALİHA EVDE UYUYUYOR"
+print(mesaj)
+mesaj1="Merhaba"
+print(mesaj1.startswith("Me"))
+print(mesaj1.endswith("a"))
+mesaj1 = mesaj1.upper()
+print(mesaj1)
+mesaj = mesaj.lower()
+print(mesaj)
+mesaj = mesaj.capitalize()
+print(len(mesaj + mesaj1))
+print(mesaj)
+isim= "Ahmet"
+yas= "17"
+print(f"{isim} , {yas} yaşındadır")
+sayı1 = 5
+sayı2 = 4.5
+print(type(sayı2))
+sayı = 7 ** 15
+print(sayı)
+sayı3 = 17 / 7
+print(sayı3)
+print(sayı * sayı3)
+print(sayı == sayı)
+print(sayı != sayı3)
