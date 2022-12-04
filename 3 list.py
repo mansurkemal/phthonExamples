b43e4cca9be67d560696e161a93e54ab3600ded3
mansurkemal <mansurkemal@hotmail.com>
2022-12-04 13:50:59 +0300

    son değişiklikler

--- 3 list.py
+++ 3 list.py
@@ -1,12 +1,41 @@
-lst = [1,2,3]
-print("sayılar")
-print(lst)
+# lst = [1,2,3]
+# print("sayılar")
+# print(lst)
 
-print("bitti")
+# print("bitti")
+# renkler = ["beyaz", "siyah", "sarı", "mavi", "yeşil"]
+# print(type(renkler))
+# print(renkler)
+# print(len(renkler))
+# print(renkler[1])
+# print(renkler[1:3:2])
+# print(renkler[1:4])
+
+# renkler = ["beyaz", "siyah", "sarı", "mavi", "yeşil"]
+# print(renkler)
+# renkler.append("gri")
+# print(renkler)
+
+# renkler = ["beyaz", "siyah", "sarı", "mavi", "yeşil"]
+# print(renkler)
+# renkler.insert(0,"mor")
+# print(renkler)
+
 renkler = ["beyaz", "siyah", "sarı", "mavi", "yeşil"]
-print(type(renkler))
 print(renkler)
-print(len(renkler))
-print(renkler[1])
-print(renkler[1:3:2])
-print(renkler[1:4])
+renkler.remove("siyah")
+print(renkler)
+renkler2 = ["mor", "turuncu"]
+renkler.extend(renkler2)
+print(renkler)
+renkler = ["beyaz", "siyah", "sarı", "mavi", "yeşil"]
+print(renkler)
+Silinen= renkler.pop()
+print(Silinen)
+print(renkler)
+silinen = renkler.pop()
+print(silinen)
+renkler = ["beyaz", "siyah", "sarı", "mavi", "yeşil"]
+print(renkler)
+renkler.reverse()
+print(renkler)
